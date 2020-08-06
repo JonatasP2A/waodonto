5d24c00617833b49bdc3b4e64b2955c695e327e5
JonatasP2A <jonatasalves@id.uff.br>
2020-08-06 12:59:51 -0300

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
 import 'react-native-gesture-handler';
 import 'intl';
 import 'intl/locale-data/jsonp/pt-BR';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, StatusBar } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 import Routes from './routes';
 import AppContainer from './hooks';
 
-const App: React.FC = () => (
-  <View style={{ flex: 1 }}>
-    <AppContainer>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      <Routes />
-    </AppContainer>
-  </View>
-);
+const App: React.FC = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+  return (
+    <View style={{ flex: 1 }}>
+      <AppContainer>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        <Routes />
+      </AppContainer>
+    </View>
+  );
+};
 
 export default App;
